c84f762a8c8743527e008fbe038f90f6af998922
wan0705 <3089755630@qq.com>
2025-07-07 21:48:53 +0800

    Site updated: 2025-07-07 21:48:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/06/hello-world/"];function toRandomPost(){
+var posts=["2025/07/07/我为什么要搭建博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
